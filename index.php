e8ef772d90c0c528ab0f014adf1c2ed0267d7a1d
zarcven <zarc771048@gmail.com>
2024-12-04 17:18:41 +0800

    Added scoring it is based on the attempts made on guessing the number. Also added username

--- index.php
+++ index.php
@@ -1,30 +1,55 @@
 <?php
 session_start();
-if (isset($_GET["n"]) && isset($_SESSION["n"]))
-{
-    $mynumber=$_SESSION["n"];
-    if($mynumber==$_GET["n"])
-    {
 
-        echo "Right Guess!";
+
+if (!isset($_SESSION["username"])) {
+    $_SESSION["username"] = ""; 
+    $_SESSION["highscore"] = PHP_INT_MAX; 
+    $_SESSION["attempts"] = 0; 
+}
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST["username"])) {
+        $_SESSION["username"] = htmlspecialchars($_POST["username"]);
+        $_SESSION["n"] = rand(0, 10); 
+        $_SESSION["attempts"] = 0; 
     }
-    else 
-    {
-        if ($mynumber>$_GET["n"])
-        {
-            echo "The number you guess is low.";
+}
+
+
+if (isset($_GET["n"]) && isset($_SESSION["n"])) {
+    $mynumber = $_SESSION["n"];
+    $_SESSION["attempts"]++; 
+
+    if ($mynumber == $_GET["n"]) {
+        echo "Right Guess, " . htmlspecialchars($_SESSION["username"]) . "!<br>";
+        echo "You guessed it in " . $_SESSION["attempts"] . " attempts.<br>";
+
+       
+        if ($_SESSION["attempts"] < $_SESSION["highscore"]) {
+            $_SESSION["highscore"] = $_SESSION["attempts"];
+            echo "Congratulations! You've set a new high score!<br>";
         }
-        else 
-        {
-            echo "The number you guess is high.";
+
+
+        $mynumber = rand(0, 10);
+        $_SESSION["n"] = $mynumber;
+        $_SESSION["attempts"] = 0; 
+    } else {
+        if ($mynumber > $_GET["n"]) {
+            echo "The number you guessed is too low.<br>";
+        } else {
+            echo "The number you guessed is too high.<br>";
         }
     }
 }
-else {
-    $mynumber=rand(0,10);
-    $_SESSION["n"]=$mynumber;
-}
 
+
+if (!empty($_SESSION["username"])) {
+    echo "Welcome, " . htmlspecialchars($_SESSION["username"]) . "!<br>";
+    echo "Your high score: " . ($_SESSION["highscore"] == PHP_INT_MAX ? "None yet" : $_SESSION["highscore"]) . " attempts.<br>";
+}
 ?>
 
 <form method="get">
