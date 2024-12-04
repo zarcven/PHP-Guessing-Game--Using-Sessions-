d944f079d98707c4240354f3fc0aafdef244947a
zarcven <zarc771048@gmail.com>
2024-12-04 16:59:24 +0800

    Added session so that it will not generate a number after every try

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?
-
-if (isset($_GET["n"])){
-
+session_start();
+if (isset($_GET["n"]) && isset($_SESSION["n"]))
+{
+    $mynumber=$_SESSION["n"];
     if($mynumber==$_GET["n"]){
 
         echo "Right Guess!";
@@ -13,12 +14,13 @@ if (isset($_GET["n"])){
 }
 else {
     $mynumber=rand(0,10);
+    $_SESSION["n"]=$mynumber;
 }
 
 ?>
 
 <form method="get">
     What is the number
-    <input name="n"/>
+    <input name="n"/><br><br>
     <button>Guess</button>
 </form>
